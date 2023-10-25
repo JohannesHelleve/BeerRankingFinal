1411818378d05890d984528f2bb411b9f3445877
Johannes Helleve <Johanshelleve@gmail.com>
2023-10-25 15:14:13 +0200

    added workflows and cleaned backend

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,74 +1,11 @@
 import { products } from '$db/products';
 import type { PageServerLoad } from './$types';
-import { KASSAL_BEARER_TOKEN } from "$env/static/private";
-import eanList from "$db/ean";
 
-
-async function updateDB(){
-    for (let i = 0; i < eanList.length; i++) {
-        const product = await getProduct(eanList[i].ean)
-        await updateProduct(product)
-    }
-}
-
-
-async function getProduct(ean: string) {
-    const response = await fetch("https://kassal.app/api/v1/products/ean/" + ean, {
-        headers: {
-            Authorization: `Bearer ${KASSAL_BEARER_TOKEN}`
-        }
-    })
-    return prettifyProduct(response);
-}
-
-async function prettifyProduct(response){
-    const responseJSON = await response.json();
-    const stores = []
-
-    for (const store of responseJSON.data.products) {
-        if (store.store == null || store.current_price == null){
-            continue
-        }
-        
-        stores.push({
-            storeName : store.store.name,
-            storePrice : store.current_price.price,
-        })
-    }
-
-    stores.sort((a, b) => (a.storePrice > b.storePrice) ? 1 : -1)
-
-    const prettyProduct = {
-        ean : responseJSON.data.ean,
-        productName : responseJSON.data.products[0].name,
-        stores : stores
-    }
-    return prettyProduct;
-}
-
-
-async function updateProduct(product) {
-    try{
-        const collection = products; 
-        const filter = { ean: product.ean };
-        await collection.findOneAndReplace(filter, product, { upsert: true });
-        return {
-            status: 200,
-        }
-
-    } catch (error) {
-        return {
-            status: 500,
-            body: error
-        }
-    }
-}
 
 
 export const load: PageServerLoad = async function() {
     const data = await products.find({}).toArray();
     const productsWithoutId = data.map(({ _id, ...rest }) => rest);
-    await updateDB();
     return {
         products: productsWithoutId
     };
