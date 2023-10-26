37082d223b927d522e02f4ef5de610a06b7ed322
Johannes Helleve <Johanshelleve@gmail.com>
2023-10-26 14:03:16 +0200

    changed env

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URL);
 
 export function start_mongo () {
     console.log("Starting mongo");
