95cbdae5d5c32ca9b83597e70c8213a6c1e020bc
Johannes Helleve <Johanshelleve@gmail.com>
2023-10-28 18:44:19 +0200

    tester om jeg kan importere env uten at vercel build ikke funker

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
-
-const client = new MongoClient(process.env.MONGO_URL);
+import { MONGO_URL } from "$env/static/private";
+const client = new MongoClient(MONGO_URL || process.env.MONGO_URL);
 
 export function start_mongo () {
     console.log("Starting mongo");
